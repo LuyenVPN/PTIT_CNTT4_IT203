b72b5b69ed5de4317ff4483cf102b35b8f926b4e
luyendv <luyen28042006@gmail.com>
2026-01-26 00:05:06 +0700

    ss1

--- src/Session1/B5_s1.java
+++ src/Session1/B5_s1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class B5_s1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Nhập mã sách 4 chữ số: ");
         int number = sc.nextInt();
 
