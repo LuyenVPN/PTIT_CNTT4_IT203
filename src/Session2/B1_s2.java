d4ffe598b5504573a58c415d0a6d8b6ba2a3b872
luyendv <luyen28042006@gmail.com>
2026-01-26 21:55:19 +0700

    ss2

--- src/Session2/B1_s2.java
+++ src/Session2/B1_s2.java
@@ -8,15 +8,15 @@ public class B1_s2 {
         System.out.print("Nhập tuổi của bạn: ");
         int age = sc.nextInt();
         System.out.print("Nhập số sách đang mượn: ");
-        int borrowedBooks = sc.nextInt();
+        int book = sc.nextInt();
 
-        if (age >= 18 && borrowedBooks < 3) {
+        if (age >= 18 && book < 3) {
             System.out.println("Cho phép mượn sách");
         } else {
             if (age < 18) {
                 System.out.println("Bạn chưa đủ tuổi");
             }
-            if (borrowedBooks >= 3) {
+            if (book >= 3) {
                 System.out.println("Bạn đã mượn quá số lượng cho phép");
             }
         }
