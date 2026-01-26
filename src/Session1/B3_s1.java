4939e2945fc35a18c82224e7b377ee22c55fea73
luyendv <luyen28042006@gmail.com>
2026-01-26 15:55:13 +0700

    ss1

--- src/Session1/B3_s1.java
+++ src/Session1/B3_s1.java
@@ -4,7 +4,6 @@ public class B3_s1 {
     public static void main(String[] args) {
         String book1 = "Java Basic";
         String book2 = "Python Intro";
-
         String temp;
         temp = book1;
         book1 = book2;
---
+++ src/Session2/B2_s2.java
@@ -0,0 +1,7 @@
+package Session2;
+
+public class B2_s2 {
+    public static void main(String[] args) {
+
+    }
+}
