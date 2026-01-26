317cc760d1ca6edd59f43a052b735d0e3198a85a
luyendv <luyen28042006@gmail.com>
2026-01-26 22:38:27 +0700

    ss2

--- src/Session2/B2_s2.java
+++ src/Session2/B2_s2.java
@@ -1,35 +1,25 @@
 package Session2;
 
+import java.util.Scanner;
+
 public class B2_s2 {
     public static void main(String[] args) {
-// 2. Mô tả
-// Hệ thống cần chỉ dẫn vị trí tầng dựa trên mã thể loại sách (A, B, C, D)
-
-//  
-
-// Yêu cầu:
-
-// Nhập một ký tự code
-// Sử dụng switch(code) để phân nhánh:
-// 'A': "Tầng 1: Sách Văn học"
-// 'B': "Tầng 2: Sách Khoa học"
-// 'C': "Tầng 3: Sách Ngoại ngữ"
-// 'D': "Tầng 4: Sách Tin học"
-// Sử dụng default để xử lý trường hợp nhập sai mã
-        char code = 'B';
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập mã thể loại sách (A, B, C, D): ");
+        char code= sc.next().charAt(0);
 
         switch (code) {
             case 'A':
-                System.out.println("Tầng 1: Sách Văn học");
+                System.out.println("Vị Trí: Tầng 1 - Sách Văn học");
                 break;
             case 'B':
-                System.out.println("Tầng 2: Sách Khoa học");
+                System.out.println("Vị Trí: Tầng 2 - Sách Khoa học");
                 break;
             case 'C':
-                System.out.println("Tầng 3: Sách Ngoại ngữ");
+                System.out.println("Vị Trí: Tầng 3 - Sách Ngoại ngữ");
                 break;
             case 'D':
-                System.out.println("Tầng 4: Sách Tin học");
+                System.out.println("Vị Trí: Tầng 4 - Sách Tin học");
                 break;
             default:
                 System.out.println("Mã thể loại sách không hợp lệ");
