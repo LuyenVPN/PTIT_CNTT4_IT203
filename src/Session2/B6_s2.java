8ebb1dd8e71d3ef87e4f989ec456883a00c55b2f
luyendv <luyen28042006@gmail.com>
2026-01-27 04:42:38 +0700

    ss2

--- src/Session2/B6_s2.java
+++ src/Session2/B6_s2.java
@@ -1,7 +1,47 @@
 package Session2;
 
+import java.util.Scanner;
+
 public class B6_s2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int max = 0;
+        int min = 0;
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 1; i <= 7; i++) {
+            System.out.print("Nhap so luot muon sach ngay thu " + i + ": ");
+            int borrow = sc.nextInt();
+
+            if (borrow == 0) {
+                continue;
+            }
+
+            if (count == 0) {
+                max = borrow;
+                min = borrow;
+            } else {
+                if (borrow > max) {
+                    max = borrow;
+                }
+                if (borrow < min) {
+                    min = borrow;
+                }
+            }
+
+            sum += borrow;
+            count++;
+        }
+
+        if (count > 0) {
+            double average = (double) sum / count;
+            System.out.println("Luot muon cao nhat: " + max);
+            System.out.println("Luot muon thap nhat: " + min);
+            System.out.println("Trung binh luot muon: " + average);
+        } else {
+            System.out.println("Khong co ngay nao mo cua");
+        }
     }
 }
