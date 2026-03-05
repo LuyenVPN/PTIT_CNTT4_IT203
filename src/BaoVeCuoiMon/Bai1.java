50c94cec7130435f82b1a46ddb9aaf24922d13d1
luyendv <luyen28042006@gmail.com>
2026-03-05 08:06:59 +0700

    SS16

---
+++ src/BaoVeCuoiMon/Bai1.java
@@ -0,0 +1,27 @@
+package BaoVeCuoiMon;
+
+public class Bai1 {
+    // Câu 1:
+    // xây dựng chương trình quản lý danh sách tên sinh viên. Người dùng nhập số lượng sinh viên và danh sách tên
+    // Yêu cầu: sử dụng List<String> để lưu dữ liệu
+    // Phải tự viết thuật toán xử lý
+    // Chương trình cần thực hiện:
+    // - Tìm tên dài nhất (nếu nhiều tên cùng độ dài, lấy tên xuất hiện đầu tiên).
+    // - Hiện thị danh sách tên theo thứ tự alphabet
+    // - Đếm số sinh vieen có tên bắt đầu bằng chữ 'A' (ko phân biệt hoa/thường).
+    //
+     // Câu 2:
+    // Xây dựng chương trình tống kê tần suất xuất hiện của các số trong một dãy số nguyên nhập từ bàn phím.
+    // Yêu cầu:
+    // - Sử dụng Map<Integer,Integer> để lưu:
+    // + Key: số nguyên
+    // +Value: số lần xuất hiện
+    //- Phải tự viết thuật toán:
+    // + Tìm số xuất hiện nhiều nhất( nếu nhiều thì liệt kê).
+    // + Sắp xếp các số theo thứ tự tăng dần.
+
+
+
+
+
+}
